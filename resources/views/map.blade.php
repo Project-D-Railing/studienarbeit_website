f8f8d32138b7f750cb3bea406b7d31c775555686
Dominik S <github@dominik-schmitt.eu>
2018-03-21 11:15:42 +0100

    Moved images of leaflet
    Due a bug i decided to move this. its not a good way but hey it works

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -14,13 +14,13 @@
 
 @section('scripts')
 
-var map = L.map('leafletmap').setView([51.505, -0.09], 13);
+var map = L.map('leafletmap').setView([49.011, 8.404], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([49.011, 8.404]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 

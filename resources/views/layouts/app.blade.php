e46d799f1fd3158a4ff0c361594945bc81a83d4e
Dominik S <github@dominik-schmitt.eu>
2018-03-24 18:47:36 +0100

    sssaass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,16 +79,6 @@
 
 <script type="text/javascript">
         @yield ('scripts')
-$( "#buttondate" ).click(function() {
-    
-    $.get(
-        "station/{{ $stationdetail->EVA_NR }}/"+document.getElementById("selecteddate").value,
-        function (data) {
-            $("#result").html(data);
-        }
-    );
-
-});
 </script>
 
 </body>

198c06904ee66647eacaa87f622abb193144b88e
Dominik S <github@dominik-schmitt.eu>
2018-03-24 18:37:00 +0100

    aaa

--- resources/views/stationdetail.blade.php
+++ resources/views/stationdetail.blade.php
@@ -23,10 +23,21 @@
         </div>
         <div id="result">
     </div>
-
+    <script type="text/javascript">
+        @yield ('scripts')
+    </script>
 @endsection
 
 @section('scripts')
 
+function myFunction() {
+    
+    $.get(
+        "station/{{ $stationdetail->EVA_NR }}/"+document.getElementById("selecteddate").value,
+        function (data) {
+            $("#result").html(data);
+        }
+    );
+}
 
 @stop

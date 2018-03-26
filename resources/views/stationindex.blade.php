0a3a39a637e1b010c3ee47a009fe748a5e2ef7a2
Dominik S <github@dominik-schmitt.eu>
2018-03-26 09:33:24 +0200

    Fixed URL

--- resources/views/stationindex.blade.php
+++ resources/views/stationindex.blade.php
@@ -51,7 +51,7 @@ jQuery(document).ready(function($) {
                         '<div class="list-group search-results-dropdown">'
                     ],
                     suggestion: function (data) {
-                        return '<a href="/station/' + data.EVA_NR + '" class="list-group-item">' + data.NAME + '</a>'
+                        return '<a href="station/' + data.EVA_NR + '" class="list-group-item">' + data.NAME + '</a>'
                     }
                 }
             });

d5263e3cc1a3746aaf86396c912e468b969e3f15
Dominik S <github@dominik-schmitt.eu>
2018-04-07 18:34:03 +0200

    New fix

--- resources/views/stationdetail.blade.php
+++ resources/views/stationdetail.blade.php
@@ -81,7 +81,12 @@ var chart = c3.generate({
 }
     
 });
-console.log({!! $zugklassen !!});
+
+@forelse($zugklassen as $zugklasse)
+console.log({{ $zugklasse }});
+@empty
+console.log('Keine Zugklassen gefunden');
+@endforelse
 var chart2 = c3.generate({
         bindto: '#chart2',
         data: {

265ad931d85bd0330e9c00dd3a4203b84a69dac9
Dominik S <github@dominik-schmitt.eu>
2018-04-08 15:12:31 +0200

    Using more cache

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Controllers\Controller;
 use Response;
 
@@ -43,17 +44,25 @@ class StationController extends Controller
     public function show($id)
     {
         $station = DB::select("select * from haltestellen2 where EVA_NR = :evanr", ['evanr' => $id]);
-        $zugklassen = DB::connection('mysql2')->select("SELECT DISTINCT(zugklasse) as name FROM zuege WHERE evanr= :evanr", ['evanr' => $id]);
+       
+        $zugklassen = Cache::remember('showstation'.$id, 30, function() use ($id){             
+            $zugklassen = DB::connection('mysql2')->select("SELECT DISTINCT(zugklasse) as name FROM zuege WHERE evanr= :evanr", ['evanr' => $id]);
+            
+            return $zugklassen;
+        }); 
+
         return view('stationdetail', ['station' => $station,'zugklassen' => $zugklassen]);
 
     }
 
     public function showdate($id, $date)
     {
-        $stationdate = DB::connection('mysql2')->select("SELECT zuege.* FROM zuege WHERE datum= :datum and zuege.evanr= :evanr", ['evanr' => $id, 'datum' => $date]);
-        //return view('stationdetail', ['stationdate' => $stationdate]);
-       
-        return view("stationdetaildate", ['zuege' => $stationdate])->render();
+        $stats = Cache::remember('somedate'.$id.'-'.$date, 30, function() use ($id, $date){             
+            $stationdate = DB::connection('mysql2')->select("SELECT zuege.* FROM zuege WHERE datum= :datum and zuege.evanr= :evanr", ['evanr' => $id, 'datum' => $date]);
+            
+            return $stationdate;
+        });       
+        return view("stationdetaildate", ['zuege' => $stats])->render();
 
     }
 

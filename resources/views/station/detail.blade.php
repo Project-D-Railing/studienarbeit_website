4ad8844c7f3838ea647dfb451429941ba3b4248b
Dominik S <github@dominik-schmitt.eu>
2018-04-17 19:33:40 +0200

    Moved gleis chart out of default details page

--- resources/views/station/detail.blade.php
+++ resources/views/station/detail.blade.php
@@ -104,40 +104,4 @@ var chart = c3.generate({
 }
     
 });
-var zugklassen = [];
-@forelse($zugklassen as $zugklasse)
-    zugklassen.push('{{ $zugklasse->name }}');
-@empty
-    console.log('Keine Zugklassen gefunden');
-@endforelse
-var chart2 = c3.generate({
-        bindto: '#chart2',
-        data: {
-            url: '{{ route('graph.trainperplatform', ['id' => $stationdetail->EVA_NR]) }}',
-            mimeType: 'json',
-            
-            keys: {
-                x: 'name', // it's possible to specify 'x' when category axis
-                value: zugklassen,
-            },
-            type: 'bar',
-            groups: [ ]
-        },
-        axis: {
-            x: {
-                type: 'category',
-                label: {
-                    text: 'Gleis',
-                    position: 'outer-center'
-                }
-            },
-            y: {
-                label: {
-                    text: 'Anzahl an Zügen',
-                    position: 'outer-middle'
-                }
-            }
-        }
-    });
-
 @stop
---
+++ resources/views/station/detailgleis.blade.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="col">
+        <div id="chartgleis">
+        </div>
+    </div>
+</div>
+
+
+
+
+@section('scripts')
+
+var zugklassen = [];
+@forelse($zugklassen as $zugklasse)
+    zugklassen.push('{{ $zugklasse->name }}');
+@empty
+    console.log('Keine Zugklassen gefunden');
+@endforelse
+var chart2 = c3.generate({
+        bindto: '#chartgleis',
+        data: {
+            url: '{{ route('graph.trainperplatform', ['id' => $stationdetail->EVA_NR]) }}',
+            mimeType: 'json',
+            
+            keys: {
+                x: 'name', // it's possible to specify 'x' when category axis
+                value: zugklassen,
+            },
+            type: 'bar',
+            groups: [ ]
+        },
+        axis: {
+            x: {
+                type: 'category',
+                label: {
+                    text: 'Gleis',
+                    position: 'outer-center'
+                }
+            },
+            y: {
+                label: {
+                    text: 'Anzahl an Zügen',
+                    position: 'outer-middle'
+                }
+            }
+        }
+    });
+
+@stop

3fdb3c0fe7c70bd010d2885d4f98403e989e363e
Dominik S <github@dominik-schmitt.eu>
2018-04-17 20:36:20 +0200

    Changed back

--- resources/views/station/detaildate.blade.php
+++ resources/views/station/detaildate.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col">
         <p>Please select a date:</p>
-            <input type="date" id="myDate">
+            <input type="date" id="myDate" value="2018-02-09">
         <table class="table">
           <thead>
             <tr>
@@ -44,6 +44,7 @@
 
 <script type="text/javascript">
 var date_input = document.getElementById('myDate');
+date_input.valueAsDate = new Date();
 
 date_input.onchange = function(){
     var d = new Date(this.value);

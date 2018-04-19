51c7ccd8d9c7ac29dcca954f8dfbb8282cd82e6f
Dominik S <github@dominik-schmitt.eu>
2018-04-19 18:23:27 +0200

    Changed Layout

--- resources/views/train/route.blade.php
+++ resources/views/train/route.blade.php
@@ -1,8 +1,15 @@
 <div class="row">
     <div class="col">
+        <br>
         @forelse($routes as $route)   
-            @forelse($route as $station)   
-                {{ $station }} ,
+            @forelse($route as $station)
+                @if($loop->last)
+                    <b>{{ $station }}</b>
+                @elseif ($loop->first)
+                    <b>{{ $station }}</b>, 
+                @else
+                    {{ $station }}, 
+                @endif
             @empty
             @endforelse
             <br><hr><br>

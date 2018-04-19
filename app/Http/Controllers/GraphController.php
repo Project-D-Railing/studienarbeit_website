08ab2b9d595daa0315e8bedbbfb5271402d773e8
Dominik S <github@dominik-schmitt.eu>
2018-04-19 19:07:34 +0200

    Array key must exist to get data

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -86,7 +86,7 @@ class GraphController extends Controller
         $trains = DB::connection('mysql2')->select("SELECT datum, evanr, arzeitsoll, arzeitist, dpzeitsoll, dpzeitist, zugstatus, NAME FROM zuege,haltestellen2 where zuege.evanr=haltestellen2.EVA_NR AND zugklasse= :zugklasse and zugnummer= :zugnummer and datum > (SELECT CURRENT_DATE - INTERVAL 14 DAY) ORDER BY id desc", ['zugklasse' => $zugklasse, 'zugnummer' => $zugnummer]);
         $trainformatted = array();
         foreach ($trains as $train) {
-            if ($trainformatted[$train->evanr] == NULL) {
+            if (!array_key_exists($train->evanr, $trainformatted)) {
                 $trainformatted[$train->evanr][] = array('x','Ankunft','Abfahrt');
             }
             if($train->zugstatus == 'n') {

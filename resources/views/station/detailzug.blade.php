4b73071474c5f0b7773d49d69e33cce73ef74c33
Dominik S <github@dominik-schmitt.eu>
2018-04-19 19:51:33 +0200

    Zugkassen statistik idee

--- resources/views/station/detailzug.blade.php
+++ resources/views/station/detailzug.blade.php
@@ -1,7 +1,35 @@
 <div class="row">
     <div class="col">
         <div id="chartzug">
-            Hier eine Andere Grafik
         </div>
     </div>
 </div>
+
+
+
+<script type="text/javascript">
+
+@forelse($station as $stationdetail)
+  @if ($loop->first) 
+    var chart = c3.generate({
+        bindto: '#chartzug',
+        data: {
+            x : 'x',
+            rows: [
+                ['x', 'RE', 'ICE'],
+                ['Zugklassen', 50, 33],
+            ],
+            type: 'bar'
+        },
+        axis: {
+            x: {
+                type: 'category' // this needed to load string x value
+            }
+        }
+    });
+  @endif
+@empty
+                   
+@endforelse
+
+</script>

804f48b75a27f0252044ab16af33a2c33faa970d
Dominik S <github@dominik-schmitt.eu>
2018-05-08 21:29:00 +0200

    Frontend

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -85,8 +85,8 @@ class StationController extends Controller
             }
             return $stationarray;
         });       
-        print_r($stats);
-        die();
+        //print_r($stats);
+        //die();
         return view("station.detaildate", ['zuege' => $stats, 'station' => $station, 'datum' => $date])->render();
 
     }
